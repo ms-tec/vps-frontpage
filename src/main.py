275d3840da6ba85f67665130005aa3f66aa1a0d5
Maya Saietz <ms@tec.dk>
2022-05-13 14:49:52 +0200

    hello, world

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import sanic
+import dominate
+from dominate.tags import *
+
+app = sanic.Sanic("TestApp")
+app.config.FORWARED_SECRET = "CookiesAreGreat"
+
+@app.get("/")
+async def frontpage(request):
+    doc = dominate.document(title='Velkommen')
+
+    with doc.head:
+        link(rel="stylesheet", href="style.css")
+
+    with doc:
+        h1("Hello, world!")
+
+    return sanic.response.html(doc.render())
+
+app.run('localhost', 8080)
